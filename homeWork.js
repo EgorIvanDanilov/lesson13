272ce98c06597efe9ffcc2d1cf2ca9c8d9ca0f48
Egor Danilov <mellowd94@gmail.com>
2023-10-11 18:54:38 +0200

    add second HomeWork

--- homeWork.js
+++ homeWork.js
@@ -12,3 +12,22 @@ const studentPromise = getStudent();
 studentPromise.then((student) => {
   console.log(student.name);
 });
+
+//2
+
+function getDriverLicense(success, failure) {
+  if (Math.random() > 0.1) {
+    success("Success!");
+  } else {
+    failure("Failure!=(");
+  }
+}
+
+function success(res) {
+  console.log(`Success! With: ${res}`);
+}
+
+function failure(msg) {
+  console.log(`Failure! with: ${msg}`);
+}
+getDriverLicense(success, failure);
